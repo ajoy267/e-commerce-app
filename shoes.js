781373ad4f2d6152d2707800eb5f3f71a6c37137
ajoy267 <andrew.joy12@hotmail.com>
2021-10-11 13:51:36 -0700

    added my array of shoes and included images

---
+++ shoes.js
@@ -0,0 +1,32 @@
+export const shoes = [
+    {
+        id: '1',
+        name: 'Nike ZoomX Vaporfly Next% 2',
+        img: './assets/next%.jpeg',
+        price: 250
+    }, 
+    {
+        id: '2',
+        name: 'Nike Air Zoom Pegasus 38',
+        img: './assets/pegasus.jpeg',
+        price: 120
+    },
+    {
+        id: '3',
+        name: 'Nike Zoom Fly 4',
+        img: './assets/zoom-fly.jpeg',
+        price: 160
+    },
+    {
+        id: '4',
+        name: 'Nike Air Zoom Tempo Next% Flyknit',
+        img: './assets/tempo.jpeg',
+        price: 200
+    },
+    {
+        id: '5',
+        name: 'Nike Pegasus Trail 3',
+        img: './assets/pegasus-trail.jpeg',
+        price: 130
+    }
+];
